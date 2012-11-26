f3d85dfb89712c9e35f079578f516b3e3698255f
Marble.Cake <hkr.drm@gmx.com>
2012-11-26 17:14:06 -0600

    put cart in checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,17 @@
 class CheckoutController < ApplicationController
   def payment
+    @cart = find_cart
   end
+  def total
+    @cart = find_cart
+    @cart.total_price
+  end
+
+private
+    def find_cart
+      unless session[:cart]
+        session[:cart] = Cart.new
+      end
+      session[:cart]
+    end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,16 +9,29 @@ class StoreController < ApplicationController
   end
 
   def add_to_cart
-    @cart = find_cart
-    product = Product.find(params[:id])
-    @cart.add_product(product)
+    begin
+      product = Product.find(params[:id])
+    rescue Mongoid::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid product") 
+    else
+      @cart = find_cart
+      @cart.add_product(product)
+    end
   end
 
-  def clear_session
+  def empty_cart 
     session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
   end
+
 
   private 
+
+    def redirect_to_index(msg)
+      flash[:notice] = msg
+      redirect_to :action => :index
+    end
 
     def find_cart
       unless session[:cart]
