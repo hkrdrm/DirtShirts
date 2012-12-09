19adbdedbd116860c22fcc260ebc3d8cd2572b49
Marble.Cake <hkr.drm@gmx.com>
2012-12-09 13:18:33 -0600

    Revert "Revert "Revert "Leaving this branch for minute"""
    This reverts commit 093d1015fe441e82172a7c20bc23ef06092e74ad.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,40 +26,6 @@ class OrdersController < ApplicationController
       format.json
     end
   end
-
-  def edit
-    @order = Order.find(params[:id])
-  end
-
-  def update
-    @order = Order.find(params[:id])
-
-    respond_to do |format|
-      if @order.update_attributes(params[:order])
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-	format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-	format.json { render json: @order.errors, status: unprocessable_entity }
-      end
-    end
-  end
-
-  def ship_order(order)
-    @order = Order.find(order.id)
-    @order.shipped = true
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to "/orders" }
-	format.json { head :no_content }
-      else
-        format.html { render action: "ship_order" }
-	format.json { render json: @order.errors, status: unprocessable_entity }
-      end
-    end
-  end
-
   protected
     def authorize
       if !user_signed_in?
